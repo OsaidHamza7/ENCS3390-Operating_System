8bcd8c57405e7f4a7fcb0de9dced7b11ade3cddf
Osaid Hamza <108472343+OsaidHamza7@users.noreply.github.com>
2022-12-13 14:06:56 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 import threading
 import time
-def EvenNumbers(a,even):
-    for i  in a:
-        if i % 2 == 0:
-            even.append(i)
-    even.sort()
-    print(f'Thread one results: {even}')
+def MultiThreading(Array1,Array2,option):
+    num=""
+    if option!="merge":
+        if option=="even":#sort even elements in Array1
+            num="one"
+            for i in Array1:
+                if i % 2 == 0:
+                    Array2.append(i)
+        elif option=="odd":#sort odd elements in Array1
+            num="two"
+            for i in Array1:
+                if i % 2 != 0:
+                    Array2.append(i)
+        Array2.sort()
+        print(f'Thread {num} results: {Array2}')
 
-
-def OddNumbers(a,odd):
-    for i  in a:
-        if i % 2 != 0:
-            odd.append(i)
-    odd.sort()
-    print(f'Thread two results: {odd}')
-
-
-def Merg(a,b,merg):
-    time.sleep(0.001)
-    merg+=a+b
-    print(f'Thread three results: {merg}')
-
+    else:#merge even array and odd array
+        merge=Array1+Array2
+        print(f'Thread three results: {merge}')
 
 even=[]
 odd=[]
-merg=[]
-a=[2,29,3,0,11,8,32,9,1,7,4,5,6,10,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,30,31]
-t1=threading.Thread(args=(a,even),target=EvenNumbers)
-t2=threading.Thread(args=(a,odd),target=OddNumbers)
-t3=threading.Thread(args=(even,odd,merg),target=Merg)
+merge=[]
+
+Array=[2,29,3,0,11,8,32,9,1,7,4,5,6,10,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,30,31]
+
+t1=threading.Thread(args=(Array,even,"even"),target=MultiThreading)
+t2=threading.Thread(args=(Array,odd,"odd"),target=MultiThreading)
+t3=threading.Thread(args=(even,odd,"merge"),target=MultiThreading)
 
 t1.start()
 t2.start()
-t3.start()
 t1.join()
 t2.join()
+t3.start()
 t3.join()
 print("Multithreading was done!")
